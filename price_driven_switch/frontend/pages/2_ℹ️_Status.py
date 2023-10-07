96014b82e7fc998821851a67da0f50d64f716a90
pavelapekhtin <pavel.apekhtin@gmail.com>
2023-10-07 19:56:16 +0200

    fix(status-page): do not show real-time info if power logic is disabled
    If Max Power in Settings tab is set to 0, power logic is disabled or real-time meter is not present, so there is no need to show the real-time power consumption

--- price_driven_switch/frontend/pages/2_ℹ️_Status.py
+++ price_driven_switch/frontend/pages/2_ℹ️_Status.py
@@ -7,35 +7,43 @@ from price_driven_switch.frontend.st_functions import (
     get_subscription_status,
 )
 
-col1, col2, col3 = st.columns(3)
+power_limit = load_settings_file().get("Settings", {}).get("MaxPower")
 
-with col1:
-    st.metric(
-        label="Subscription Status",
-        value=get_subscription_status(),
-    )
+# show subscription status, power now, power limit if power limit logic is enabled
+if power_limit > 0:
+    col1, col2, col3 = st.columns(3)
 
-with col2:
-    st.metric(
-        label="Power Now, kW",
-        value=round(get_power_reading() / 1000, 3)  # type: ignore
-        if isinstance(get_power_reading(), int)
-        else get_power_reading(),
-        delta=round(
-            round(get_power_reading() / 1000, 3)  # type: ignore
-            - round(load_settings_file().get("Settings", {}).get("MaxPower"), 3),
-            3,
-        ),
-        delta_color="inverse",
-    )
-with col3:
-    st.metric(
-        label="Power Limit, kW",
-        value=round(load_settings_file().get("Settings", {}).get("MaxPower"), 2),
-    )
+    with col1:
+        st.metric(
+            label="Subscription Status",
+            value=get_subscription_status(),
+        )
 
-if st.button("Refresh"):
-    st.rerun()
+    with col2:
+        st.metric(
+            label="Power Now, kW",
+            value=round(get_power_reading() / 1000, 3)  # type: ignore
+            if isinstance(get_power_reading(), int)
+            else get_power_reading(),
+            delta=round(
+                round(get_power_reading() / 1000, 3)  # type: ignore
+                - round(power_limit, 3),
+                3,
+            ),
+            delta_color="inverse",
+        )
+    with col3:
+        st.metric(
+            label="Power Limit, kW",
+            value=round(power_limit, 2),
+        )
+
+    if st.button("Refresh"):
+        st.rerun()
+else:
+    st.write(
+        "Power Limit Logic is disabled. Set Max Power above 0 in Settings if you have Tibber Pulse or Watty connceted."
+    )
 
 st.divider()
 
