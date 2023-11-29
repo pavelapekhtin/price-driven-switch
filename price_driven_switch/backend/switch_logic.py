42718d816e28d1b26f08be8dfdadf9a5a0c1cbbf
pavelapekhtin <pavel.apekhtin@gmail.com>
2023-11-30 00:00:27 +0100

    fix(power_limit): fixed staying on despite the price in some cases
    If previous state of the appliance was on and price limit for it changed to off, it would not be turned off

--- price_driven_switch/backend/switch_logic.py
+++ price_driven_switch/backend/switch_logic.py
@@ -66,11 +66,13 @@ def limit_power(
 
             logger.info(f"[OVER] {power_now - power_limit * 1000} W , reducing power.")
             for priority in range(0, switch_df["Priority"].max() + 1):
-                for index, row in switch_df.iterrows():
-                    if row["Priority"] == priority and row["on"] is True:
-                        total_power = total_power - row["Power"] * 1000
+                for index, prev_row in switch_df.iterrows():
+                    if prev_row["Priority"] == priority and prev_row["on"] is True:
+                        total_power = total_power - prev_row["Power"] * 1000
                         switch_df.at[index, "on"] = False
-                        logger.info(f"Turning off {index}, power {row['Power']} kW")
+                        logger.info(
+                            f"Turning off {index}, power {prev_row['Power']} kW"
+                        )
                         logger.debug(f"Estimated total power now {total_power} kW")
                         if total_power < power_limit * 1000:
                             break
@@ -82,32 +84,33 @@ def limit_power(
     elif check_frames(switch_df, prev_states_df, "on") and not switch_df.equals(
         prev_states_df
     ):
-        logger.debug("There is a previous state, using it")
+        logger.info(f"EXISTING STATE: {prev_states_df['on'].to_list()}")
         if power_now < power_limit * 1000:
             power_reserve = power_limit * 1000 - power_now
             logger.info(
                 f"[RESERVE] {power_reserve} W, checking if any appliances can be turned on"
             )
             for priority in range(0, prev_states_df["Priority"].max() + 1):
-                for index, row in prev_states_df.iterrows():
+                for index, prev_row in prev_states_df.iterrows():
                     # check if appliance was on in swtich_df but is off in prev_states_df
                     if (
-                        row["Priority"] == priority
-                        and row["on"] is False
-                        and row["on"] != switch_df.at[index, "on"]
+                        (prev_row["Priority"] == priority)
+                        and (prev_row["on"] == False)
+                        and (switch_df.at[index, "on"] == True)
                     ):
-                        total_power = power_now + row["Power"] * 1000
-                        if (
-                            total_power < power_limit * 1000
-                            and switch_df.at[index, "Power"] < power_reserve / 1000
-                        ):
+                        total_power = power_now + prev_row["Power"] * 1000
+                        if switch_df.at[index, "Power"] < power_reserve / 1000:
                             prev_states_df.at[index, "on"] = True
-                            power_reserve = power_reserve - row["Power"] * 1000
-                            logger.info(f"Turning ON {index}, power {row['Power']} kW")
+                            power_reserve = power_reserve - prev_row["Power"] * 1000
+                            logger.info(
+                                f"Turning ON {index}, power {prev_row['Power']} kW"
+                            )
                             logger.debug(
                                 f"Estimated total power now {total_power/ 1000} kW"
                             )
                             logger.debug(f"Power reserve now {power_reserve / 1000} kW")
+                    if switch_df.at[index, "on"] == False:
+                        prev_states_df.at[index, "on"] = False
             logger.debug("No more appliances can be turned on")
             return prev_states_df
 
@@ -117,11 +120,13 @@ def limit_power(
             logger.info(f"[OVER] {total_power - power_limit * 1000} W)")
             logger.info(f"Previous state: {prev_states_df['on'].to_list()}")
             for priority in range(0, switch_df["Priority"].max() + 1):
-                for index, row in prev_states_df.iterrows():
-                    if row["Priority"] == priority and row["on"] is True:
-                        total_power = total_power - row["Power"] * 1000
+                for index, prev_row in prev_states_df.iterrows():
+                    if prev_row["Priority"] == priority and prev_row["on"] is True:
+                        total_power = total_power - prev_row["Power"] * 1000
                         prev_states_df.at[index, "on"] = False
-                        logger.info(f"Turning OFF {index}, power {row['Power']} kW")
+                        logger.info(
+                            f"Turning OFF {index}, power {prev_row['Power']} kW"
+                        )
                         logger.debug(
                             f"Estimated total power now {total_power / 1000} kW"
                         )
